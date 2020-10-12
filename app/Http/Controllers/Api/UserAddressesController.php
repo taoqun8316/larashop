0a3b07f4677f9f83e3b2014f8ec8960bd766c615
taoqun <taoqun8316@163.com>
2020-10-13 01:03:17 +0800

    add

---
+++ app/Http/Controllers/Api/UserAddressesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+class UserAddressesController extends Controller
+{
+    public function index(Request $request)
+    {
+        return $this->success($request->user()->addresses);
+    }
+}
