fe8f21cb31db5f936bac6ac4285acda15d8049b8
taoqun <taoqun8316@163.com>
2020-10-13 22:33:54 +0800

    add

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use JWTAuth;
 use App\Models\User;
-use http\Env\Request;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class UserController extends Controller
 {
@@ -22,36 +24,25 @@ class UserController extends Controller
 
         $user = new User();
         $user->name = $request->name;
-        $user->email = $request->email;
         $user->password = password($request->password);
         $user->save();
 
         if ($this->loginAfterSignUp) {
             return $this->login($request);
         }
-
-        return response()->json([
-            'success' => true,
-            'data' => $user
-        ], 200);
+        return $this->success($user);
     }
 
     public function login(Request $request)
     {
-        $input = $request->only('email', 'password');
+        $input = $request->only('name', 'password');
         $jwt_token = null;
 
         if (!$jwt_token = JWTAuth::attempt($input)) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Invalid Email or Password',
-            ], 401);
+            return $this->failed('Invalid Email or Password');
         }
 
-        return response()->json([
-            'success' => true,
-            'token' => $jwt_token,
-        ]);
+        return $this->success($jwt_token);
     }
 
     public function logout(Request $request)
@@ -59,19 +50,11 @@ class UserController extends Controller
         $this->validate($request, [
             'token' => 'required'
         ]);
-
         try {
             JWTAuth::invalidate($request->token);
-
-            return response()->json([
-                'success' => true,
-                'message' => 'User logged out successfully'
-            ]);
+            return $this->message('User logged out successfully');
         } catch (JWTException $exception) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Sorry, the user cannot be logged out'
-            ], 500);
+            return $this->failed('Sorry, the user cannot be logged out');
         }
     }
 
@@ -80,9 +63,7 @@ class UserController extends Controller
         $this->validate($request, [
             'token' => 'required'
         ]);
-
         $user = JWTAuth::authenticate($request->token);
-
-        return response()->json(['user' => $user]);
+        return $this->success(['user' => $user]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register','UserController@register');   //注册
-Route::post('/login','UserController@login');    //登录
+Route::namespace('Api')->group(function () {
+    Route::post('/register','UserController@register');   //注册
+    Route::post('/login','UserController@login');    //登录
+});
 
-Route::namespace('Api')->group(['middleware' => 'auth.jwt'], function () {
+/*Route::namespace('Api')->group(['middleware' => 'auth.jwt'], function () {
     Route::get('/', 'PagesController@root');
     Route::get('/users','UserController@show');   //登录用户信息
     Route::get('user_addresses', 'UserAddressesController@index');  //收货地址列表
 
-});
+});*/
 
 
 
